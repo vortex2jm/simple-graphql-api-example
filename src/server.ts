df8c274f06cb016e222c28fd527f1401650a6d6b
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-23 12:01:17 -0300

    :recycle: Adding mutation and user entity

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,54 @@
 import { ApolloServer, gql} from 'apollo-server';
+import { randomUUID } from 'node:crypto'
+ 
+interface User {
+  id: String
+  name: String
+}
+
+const users: User[] = []
+
+// ==================================================== //
 
 const typeDefs = gql `
+
+  type User {
+    id: String!
+    name: String!
+  }
+
   type Query {
-    text1: String!
+    users: [User!]!
+  }
+
+  type Mutation {
+    createUser(name: String): User!
   }
 `
 
 const server = new ApolloServer({
   typeDefs,
+
   resolvers: {
+    
     Query: {
-      text1: () => {
-        return "Hello world"
+      users: () => {
+        return users
       }
+    },
+
+    Mutation: {
+
+        createUser: (parents, args, ctx) => {
+          const user: User = {
+            id: randomUUID(),
+            name: args.name 
+          }
+          users.push(user);
+          return user
+        }
     }
   } 
-
 });
 
 server.listen().then(({url}) => {
